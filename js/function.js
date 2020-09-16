ccc2858257b66c2ab7bfe6e37509345b9707582a
Wine Kittipop <37920511+kittipop1998@users.noreply.github.com>
2020-09-16 14:14:27 +0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,22 +1,22 @@
-function add(a, b) { 
-    this.user = 'wine'
-    return a + b
-}
+// function add(a, b) { 
+//     this.user = 'wine'
+//     return a + b
+// }
 
-console.log()
+// console.log()
 
-function mul(a, b) {
-    return a * b
-}
+// function mul(a, b) {
+//     return a * b
+// }
 
-function searchCustomer(customerName){
-    // find some customer
-    return {
-        name: "customar"
-    }  
-}
+// ftunction searchCustomer(customerName){
+//     // find some customer
+//     reurn {
+//         name: "customar"
+//     }  
+// }
 const add = (a,b) => a + b
 
-this.user = ???
+//this.user = ???
 
 console.log(add(2, 3))
