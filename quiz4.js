f44495fe1330351b24906b1a0f712a13c6bbce20
60023180 <32890847+60023180@users.noreply.github.com>
2020-09-16 15:56:44 +0700

    commit

---
+++ quiz4.js
@@ -0,0 +1,11 @@
+var txt = "Hello my name is Joke";
+var sln = txt.length;
+
+search()
+var str = "Hello my name is Joke nice to meet you!";
+var pos = str.search("Joke");
+
+index0f()
+var str = "Well open up your mind and see like me";
+var pos = str.index0f("open");
+
