01edebeea558a718e0861b3518a89be03c86e6ff
60023180 <32890847+60023180@users.noreply.github.com>
2020-09-16 13:43:24 +0700

    com

---
+++ js/example.js
@@ -0,0 +1,22 @@
+let name = "John"
+name = "James"
+let test = "Hello"
+name =  `${test} B`
+//let age//
+//age = 12//
+let arr = ['apple', 'banana', 'kiwi']
+
+let person ={
+    name = "John",
+    age = 15,
+    gender: {
+        name = 'male'
+    },
+    arr
+}
+console.log(person)
+//console.log("arr: ", arr[1])//
+//console.log("PI", PI)//
+//console.log("name: ", name)//
+//console.log("age", age)//
+//console.log("person: ", gender)//
