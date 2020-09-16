5a6817484f2f880f175b5fc8f70bcb88f25230f4
60023180 <32890847+60023180@users.noreply.github.com>
2020-09-16 15:59:01 +0700

    commit

--- syncAndAsync.js
+++ syncAndAsync.js
@@ -3,7 +3,7 @@ console.log("2")
 new Promise((resolve, reject ) => {
     setTimeout(() => {
         resolve(console.log("3"))
-    }, 1000);
+    }, 1000)
 }).then(() => {
     console.log("4")
     console.log("5")
