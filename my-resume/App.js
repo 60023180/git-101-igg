8c7f7c8ba7e2f37c9067e166b3cdf791a1277637
60023180 <32890847+60023180@users.noreply.github.com>
2020-09-18 15:24:14 +0700

    commit

--- my-resume/App.js
+++ my-resume/App.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { StyleSheet, View, Text, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ResumeFormScreen from './screens/ResumeForm';
-
+import ResumeFormScreen from './screens/ResumeForm'
+import ResumeDetailScreen from './screens/ResumeDetail'
 const Stack = createStackNavigator();
 
 function App() {
@@ -12,6 +12,8 @@ function App() {
       <Stack.Navigator>
         {/* add */}
         <Stack.Screen name="ResumeForm" options={{ title: 'Home ResumeForm'}} component={ResumeFormScreen}  />
+
+        <Stack.Screen name="ResumeDetail" options={{ title: 'Home ResumeDetail'}} component={ResumeDetailScreen}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- my-resume/screens/ResumeForm.js
+++ my-resume/screens/ResumeForm.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ValidationComponent from 'react-native-form-validator'
-import {StyleSheet, View, Text, TextInput, input, Button} from 'react-native'
-
+import {StyleSheet, View, Text, TextInput, input, Button, Alert} from 'react-native'
+import axios from 'axios'
+import Camera from '../components/Camera'
 
 export default class ResumeForm extends ValidationComponent {
     state = {
@@ -12,22 +13,52 @@ export default class ResumeForm extends ValidationComponent {
     }
 
     _OnSubmit = () => {
-        this.validate({
+        const isValid = this.validate({
             name: { required: true},
             nickname: { required: true},
             age: { required: true, numbers: true},
             skill: { required: true},
-        })
+        });
+        if (isValid) {
+            const formData = new FormData();
+            formData.append('name', this.state.name)
+            formData.append('nickname', this.state.nickname)
+            formData.append('age', this.state.age)
+            formData.append('skill', this.state.skill)
+            const config = {
+                headers: { 'content-type': 'multipart/form-data' }
+            }
+            axios.post(
+                'https://movie-api.igeargeek.com/users/register', formData, config
+            ).then((response) => {
+                Alert.alert(
+                    'Create success',
+                    'Click Ok go to resume detail page',
+                    [{
+                        test: 'Ok',
+                        onPress: () => {
+                            this.props.navigation.push('ResumeDetail', { id : response.data.id })
+                        }
+                    }]
+                )
+                
+            }).catch((error) => {
+                console.log('api error', error)
+            })
+
+        }
     }
 
     render () {
         return(
         <View style={styles.container}>
+            <Camera/>
             <View>
                 <Text style={styles.getErrorMessages}>
                     {this.getErrorMessages()}
                 </Text>
             </View>
+            
             <View>
                 <Text>Full name</Text>
                     <TextInput 
